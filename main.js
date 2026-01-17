161b6f20eea14f2ae09d74ddef2480c57650a994
= <shivampathak19972@gmail.com>
2026-01-17 23:07:15 +0530

    Test

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+const readline = require('readline');
 
 const csvFilePath = process.argv[2];
 const columnName = process.argv[3];
 
 let sum = 0;
+let columnIndex = -1;
+let isHeader = true;
 
-fs.createReadStream(csvFilePath)
-  .pipe(csv())
-  .on('data', (data) => {
-    // TODO: Add code to sum values in the specified column
-  })
-  .on('end', () => {
-    console.log(`The sum of ${columnName} is: ${sum}`);
-  });
+if (!csvFilePath || !columnName) {
+  console.error('Error: CSV file path and column name are required');
+  process.exit(1);
+}
+
+const stream = fs.createReadStream(csvFilePath);
+
+stream.on('error', () => {
+  console.error('Error: Invalid file path');
+  process.exit(1);
+});
+
+const rl = readline.createInterface({
+  input: stream,
+  crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+  const values = line.split(',');
+
+  if (isHeader) {
+    columnIndex = values.indexOf(columnName);
+    isHeader = false;
+    return;
+  }
+
+  if (columnIndex !== -1) {
+    const value = parseFloat(values[columnIndex]);
+    if (!isNaN(value)) {
+      sum += value;
+    }
+  }
+});
+
+rl.on('close', () => {
+  if (columnIndex === -1) {
+    console.log(0);
+  } else {
+    console.log(sum);
+  }
+});
